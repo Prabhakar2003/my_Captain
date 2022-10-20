19b75a0a82d4565bd1f41eae80054e5db6fb8b58
Prabhakarraj <113048622+Prabhakar2003@users.noreply.github.com>
2022-10-20 17:03:13 +0530

    Add files via upload

---
+++ Fibonacci numbers.py
@@ -0,0 +1,14 @@
+#   Fibonacci numbers
+
+var1 = 0
+var2 = 1
+print(var1)
+print(var2)
+while var1 <= 10000:
+    var3 = var2 + var1
+    var1 = var2
+    var2 = var3
+   
+    print(var3)
+   
+    
