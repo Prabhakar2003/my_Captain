23a53c8b7866f01ffd0737ca7ce2240dc5b105dd
Prabhakarraj <113048622+Prabhakar2003@users.noreply.github.com>
2022-10-20 18:23:45 +0530

    Add files via upload

---
+++ positive__numbers.py
@@ -0,0 +1,17 @@
+# list 1
+list1 = [4,-2,16,20,-24,3,-8,85,74,14,25,35,-454]
+
+for i in list1:
+    if i<0:
+        list1.remove(i)
+print(list1)
+
+
+#  list 2
+
+list2 = [2,5,8,-6,25,-41,54,-21,62]
+for k in list2:
+    if k < 0:
+        list2.remove(k)
+print(list2)
+
